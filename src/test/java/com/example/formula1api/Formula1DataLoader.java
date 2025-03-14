aa9c85d956ce97a0128d8619a8c37ed74c727077
David Almonacid <david.almonacidf@gmail.com>
2025-03-14 18:49:58 -0500

    Refactor Formula1DataLoader to streamline team and driver creation and saving

--- src/test/java/com/example/formula1api/Formula1DataLoader.java
+++ src/test/java/com/example/formula1api/Formula1DataLoader.java
@@ -7,8 +7,6 @@ import com.example.formula1api.team.TeamRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-
 @Component
 public class Formula1DataLoader implements CommandLineRunner {
 
@@ -22,42 +20,17 @@ public class Formula1DataLoader implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-        List<Team> teams = List.of(
-                new Team("Williams"),
-                new Team("McLaren"),
-                new Team("Ferrari"),
-                new Team("Mercedes"),
-                new Team("Red Bull Racing"),
-                new Team("Aston Martin"),
-                new Team("Alpine"),
-                new Team("AlphaTauri"),
-                new Team("Alfa Romeo Racing"),
-                new Team("Haas F1 Team")
-        );
-        teamRepository.saveAll(teams);
+        // Create and save teams
+        Team ferrari = new Team("Ferrari");
+        Team mclaren = new Team("McLaren");
+        teamRepository.save(ferrari);
+        teamRepository.save(mclaren);
 
-        driverRepository.saveAll(List.of(
-                new Driver("George Russell", teams.get(0)),
-                new Driver("Nicholas Latifi", teams.get(0)),
-                new Driver("Lando Norris", teams.get(1)),
-                new Driver("Daniel Ricciardo", teams.get(1)),
-                new Driver("Charles Leclerc", teams.get(2)),
-                new Driver("Carlos Sainz", teams.get(2)),
-                new Driver("Lewis Hamilton", teams.get(3)),
-                new Driver("Valtteri Bottas", teams.get(3)),
-                new Driver("Max Verstappen", teams.get(4)),
-                new Driver("Sergio Perez", teams.get(4)),
-                new Driver("Sebastian Vettel", teams.get(5)),
-                new Driver("Lance Stroll", teams.get(5)),
-                new Driver("Fernando Alonso", teams.get(6)),
-                new Driver("Esteban Ocon", teams.get(6)),
-                new Driver("Pierre Gasly", teams.get(7)),
-                new Driver("Yuki Tsunoda", teams.get(7)),
-                new Driver("Kimi Raikkonen", teams.get(8)),
-                new Driver("Antonio Giovinazzi", teams.get(8)),
-                new Driver("Mick Schumacher", teams.get(9)),
-                new Driver("Nikita Mazepin", teams.get(9))
-        ));
+        // Create and save drivers with the saved teams
+        Driver driver1 = new Driver("Charles Leclerc", ferrari);
+        Driver driver2 = new Driver("Oscar Piastri", mclaren);
+        driverRepository.save(driver1);
+        driverRepository.save(driver2);
     }
 
 }
