6e0693bcea92a85b733f91b47d7d97886ea0f34a
David Almonacid <david.almonacidf@gmail.com>
2025-03-27 22:16:43 -0500

    Add full_name column to teams and establish foreign key constraints for drivers and race_participants

--- src/main/resources/db/migration/V1__Initial_schema.sql
+++ src/main/resources/db/migration/V1__Initial_schema.sql
@@ -1,13 +1,13 @@
 CREATE TABLE teams (
   id bigint AUTO_INCREMENT PRIMARY KEY,
-  name varchar(255) NOT NULL UNIQUE
+  name varchar(255) NOT NULL UNIQUE,
+  full_name varchar(255) NOT NULL UNIQUE
 );
 
 CREATE TABLE drivers (
   id bigint AUTO_INCREMENT PRIMARY KEY,
   name varchar(255) NOT NULL,
-  team_id bigint,
-  FOREIGN KEY (team_id) REFERENCES teams(id)
+  team_id bigint
 );
 
 CREATE TABLE races (
@@ -18,7 +18,5 @@ CREATE TABLE races (
 
 CREATE TABLE race_participants (
   race_id bigint NOT NULL,
-  driver_id bigint NOT NULL,
-  FOREIGN KEY (driver_id) REFERENCES drivers(id),
-  FOREIGN KEY (race_id) REFERENCES races(id)
+  driver_id bigint NOT NULL
 );
--- src/main/resources/db/migration/V2__Add_team_full_name.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE teams
-ADD COLUMN full_name varchar(255) NOT NULL UNIQUE;
