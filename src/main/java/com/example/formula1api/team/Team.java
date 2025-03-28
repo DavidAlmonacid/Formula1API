ac85b77e29a6865ee42f9df3b45928c3dab4cdf5
David Almonacid <david.almonacidf@gmail.com>
2025-03-27 22:14:00 -0500

    Update Team entity to enable cascading and orphan removal for drivers

--- src/main/java/com/example/formula1api/team/Team.java
+++ src/main/java/com/example/formula1api/team/Team.java
@@ -29,7 +29,7 @@ public class Team {
     @Column(unique = true)
     private String fullName;
 
-    @OneToMany(mappedBy = "team")
+    @OneToMany(mappedBy = "team", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Driver> drivers;
 
     // Constructors
