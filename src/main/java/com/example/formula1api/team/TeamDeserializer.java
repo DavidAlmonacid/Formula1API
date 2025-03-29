e21ef4293b5c9b5d7ea6b87fb1713806655111b1
David Almonacid <david.almonacidf@gmail.com>
2025-03-28 19:46:55 -0500

    Add custom deserializer for Team entity

---
+++ src/main/java/com/example/formula1api/team/TeamDeserializer.java
@@ -0,0 +1,27 @@
+package com.example.formula1api.team;
+
+import com.fasterxml.jackson.core.JacksonException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonNode;
+
+import java.io.IOException;
+
+public class TeamDeserializer extends JsonDeserializer<Team> {
+
+    @Override
+    public Team deserialize(JsonParser p, DeserializationContext ctxt) throws IOException, JacksonException {
+        if (p.getCurrentToken() == JsonToken.VALUE_STRING) {
+            var teamName = p.getText();
+            return new Team(teamName, teamName);
+        }
+
+        var node = (JsonNode) p.getCodec().readTree(p);
+        var name = node.has("name") ? node.get("name").asText() : "";
+        var fullName = node.has("fullName") ? node.get("fullName").asText() : name;
+
+        return new Team(name, fullName);
+    }
+}
